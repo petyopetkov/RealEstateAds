0496f0328a82f501a015c0b59be9df9efff0ed12
petyopetkov <petyo.petkov@mail.bg>
2016-03-27 23:23:45 +0300

    Get User details implemented

---
+++ RealEstateAds/RealEstateAds.Api/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿namespace RealEstateAds.Api.Controllers
+{
+    using System.Web.Http;
+
+    using Models.Users;
+    using RealEstateAds.Services.Contracts;
+    
+    public class UsersController : BaseController
+    {
+        private readonly IUsersServices users;
+
+        public UsersController(IUsersServices users)
+        {
+            this.users = users;
+        }
+
+        public IHttpActionResult Get(string id)
+        {
+            var user = this.users.GetUserByUserName(id);
+            if (user == null)
+            {
+                return this.NotFound();
+            }
+
+            var result = this.Mapper.Map<UserResponceModel>(user);
+            result.CommentsCount = user.Comments.Count;
+            result.RealEstatesCount = user.RealEstates.Count;
+
+            return this.Ok(result);
+        }
+    }
+}
