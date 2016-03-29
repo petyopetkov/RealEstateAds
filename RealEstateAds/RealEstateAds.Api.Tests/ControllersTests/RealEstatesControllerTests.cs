b6529dff81542595d84ed433eb3e03631f5cbef3
petyopetkov <petyo.petkov@mail.bg>
2016-03-29 11:01:41 +0300

    Added tests for RealEstatesController Post action;

---
+++ RealEstateAds/RealEstateAds.Api.Tests/ControllersTests/RealEstatesControllerTests.cs
@@ -0,0 +1,66 @@
+﻿namespace RealEstateAds.Api.Tests.ControllersTests
+{
+    using System.Web.Http;
+    using System.Web.Http.Results;
+
+    using Controllers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    [TestClass]
+    public class RealEstatesControllerTests
+    {
+        [TestMethod]
+        public void PostShouldValidateInvalidModel()
+        {
+            var controller = new RealEstatesController(TestObjectFactory.GetRealEstatesServices());
+            controller.Configuration = new HttpConfiguration();
+
+            var model = TestObjectFactory.GetInvalidModel();
+
+            controller.Validate(model);
+            Assert.IsFalse(controller.ModelState.IsValid);
+        }
+
+        [TestMethod]
+        public void PostShouldValidateValidModel()
+        {
+            var controller = new RealEstatesController(TestObjectFactory.GetRealEstatesServices());
+            controller.Configuration = new HttpConfiguration();
+
+            var model = TestObjectFactory.GetValidModel();
+
+            controller.Validate(model);
+            Assert.IsTrue(controller.ModelState.IsValid);
+        }
+
+        [TestMethod]
+        public void PostShouldReturnBadRequestWithInvalidModel()
+        {
+            var controller = new RealEstatesController(TestObjectFactory.GetRealEstatesServices());
+            controller.Configuration = new HttpConfiguration();
+
+            var model = TestObjectFactory.GetInvalidModel();
+
+            controller.Validate(model);
+            var result = controller.Post(model);
+
+            Assert.AreEqual(typeof(InvalidModelStateResult), result.GetType());
+        }
+
+        [TestMethod]
+        public void PostShouldReturnBadRequestWithInvalidModelByPrice()
+        {
+            var controller = new RealEstatesController(TestObjectFactory.GetRealEstatesServices());
+            controller.Configuration = new HttpConfiguration();
+
+            var model = TestObjectFactory.GetInvalidModelByPrice();
+
+            controller.Validate(model);
+            var result = controller.Post(model);
+
+            Assert.AreEqual(typeof(InvalidModelStateResult), result.GetType());
+        }
+
+        
+    }
+}
---
+++ RealEstateAds/RealEstateAds.Api.Tests/TestObjectFactory.cs
@@ -0,0 +1,83 @@
+﻿namespace RealEstateAds.Api.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Moq;
+    using RealEstateAds.Models;
+    using RealEstateAds.Services.Contracts;
+    using Models.RealEstates;
+    public static class TestObjectFactory
+    {
+        private static IQueryable<RealEstate> realEstates = new List<RealEstate>
+        {
+            new RealEstate
+            {
+                Id = 1,
+                CreatedOn = new DateTime(2016, 3, 1, 10, 10, 10, 10),
+                Description = "Test Description",
+                Title = "Test title",
+                ConstructionYear = 2000,
+                Address = "Address Test",
+                Contact = "555-555-555",
+                Type = 0,
+                CanBeRented = true,
+                CanBeSold = true,
+                RentingPrice = 500,
+                SellingPrice = 100000,
+                AuthorId = "97bfc79b-896b-42a9-b20e-efff78508689"
+            }
+        }.AsQueryable();
+
+        public static IRealEstatesServices GetRealEstatesServices()
+        {
+            var realEstatesServices = new Mock<IRealEstatesServices>();
+
+            realEstatesServices.Setup(r => r.GetAll(It.IsAny<int>(), It.IsAny<int>())).Returns(realEstates);
+
+            realEstatesServices.Setup(r => r.GetById(It.IsAny<int>())).Returns(realEstates.First);
+
+            return realEstatesServices.Object;
+        }
+
+        public static RealEstateRequestModel GetInvalidModel()
+        {
+            var model = new RealEstateRequestModel { Title = "Test Title" };
+
+            return model;
+        }
+
+        public static RealEstateRequestModel GetValidModel()
+        {
+            var model = new RealEstateRequestModel
+            {
+                Title = "Test Title",
+                Description = "Test description",
+                Address = "Test address",
+                Contact = "666-666-666",
+                ConstructionYear = 2000,
+                RentingPrice = 500,
+                SellingPrice = 100000,
+                Type = 0,                
+            };
+
+            return model;
+        }
+
+        public static RealEstateRequestModel GetInvalidModelByPrice()
+        {
+            var model = new RealEstateRequestModel
+            {
+                Title = "Test Title",
+                Description = "Test description",
+                Address = "Test address",
+                Contact = "666-666-666",
+                ConstructionYear = 2000,
+                Type = 0
+            };
+
+            return model;
+        }
+    }
+}
